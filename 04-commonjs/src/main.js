ab6a8eeeb10378811cbe2f9cb366a6dd532f10de
Lucioafk <luciogabriel1550@gmail.com>
2026-03-04 20:58:53 -0300

    aprendendo a exportar em commonjs

--- 04-commonjs/src/main.js
+++ 04-commonjs/src/main.js
@@ -1,5 +1,14 @@
+ const p = require("./services/products")
+ const config = require("./services/config")
+ 
  async function main(params) {
-    console.log("Olá, mundo!")
+   console.log("Olá, mundo!")
+   p.getFullName("408", "mousepad") 
+   p.getFullName("508", "mousepad") 
+
+   p.getProductLabel("mousepad")
+
+   console.log(config.devArea);
  }
 
  main();
--- 04-commonjs/src/services/products.js
+++ 04-commonjs/src/services/products.js
@@ -1,3 +1,13 @@
 async function getFullName(codeId, productName) {
-    return codeId + "--" + productName; 
-} 
+    console.log("\n");
+    console.log("product: " + codeId + "--" + productName); 
+};
+
+async function getProductLabel(productName) {
+    console.log("Product" + productName)
+}
+
+module.exports = { 
+    getFullName,
+    getProductLabel,
+};
